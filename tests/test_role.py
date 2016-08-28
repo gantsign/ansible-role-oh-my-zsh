ae8a94ceabf4e161311c70a2abbdadeed3dd7609
John Freeman <john.freeman.opensource@gmail.com>
2016-08-28 22:28:30 +0100

    Specify hosts to testinfa via python
    Was specifying hosts via molecule.yml file. The new approach is dynamic and avoids repeating configuration information.

--- tests/test_role.py
+++ tests/test_role.py
@@ -1,5 +1,10 @@
 import pytest
 
+from testinfra.utils.ansible_runner import AnsibleRunner
+
+testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
+
+
 @pytest.mark.parametrize('username', [
     ('test_usr1'),
     ('test_usr2'),
