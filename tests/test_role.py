842589273ef6019600d1670695da8c505d528b7d
John Freeman <john.freeman.opensource@gmail.com>
2016-09-06 09:13:49 +0100

    Upgraded to Molecule 1.10.3 (#21)
    Fixed flake8 linter and ansible-lint warnings.

--- tests/test_role.py
+++ tests/test_role.py
@@ -16,6 +16,7 @@ def test_oh_my_zsh_install(File, username):
     assert oh_my_zsh.user == username
     assert oh_my_zsh.group == username
 
+
 @pytest.mark.parametrize('username,theme,plugins', [
     ('test_usr1', 'test_theme1', 'test_plugin1 test_plugin2'),
     ('test_usr2', 'test_theme2', 'test_plugin3 test_plugin4'),
@@ -28,6 +29,7 @@ def test_oh_my_zsh_config(File, username, theme, plugins):
     assert zshrc.group == username
     assert zshrc.contains(theme)
     assert zshrc.contains(plugins)
+
 
 def test_console_setup(File):
     setup = File('/etc/default/console-setup')
